3d8f8c2024f897498c39cab314f8520014fb000f
DUCANH <zjnkill18@gmail.com>
2023-07-21 12:33:09 +0700

    ud

--- src/pages/AuthPage/Register/Register.jsx
+++ src/pages/AuthPage/Register/Register.jsx
@@ -2,17 +2,21 @@ import { Button, Checkbox, Col, Divider, Form, Input, Row, message } from 'antd'
 import '../authpage.scss'
 import { FacebookOutlined } from '@ant-design/icons';
 import { handleRegister } from '../../../service/api';
-
+import { useState } from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
 
 
 
 
 const Register = (props) => {
     const { setIndexActive } = props
+    const [isLoading, setIsLoading] = useState(false)
 
     const onFinish = async (values) => {
         const { name, email, password } = values
+        setIsLoading(true)
         let res = await handleRegister(name, email, password)
+        setIsLoading(false)
         console.log(res)
         if (res && res.data) {
             setIndexActive(1)
@@ -84,7 +88,7 @@ const Register = (props) => {
                         <Col span={17} style={{ margin: '0 auto' }}>
                             <Button className='btn-login' size='large' style={{ margin: '0 auto', width: '100%', height: 45, backgroundColor: '#C92127' }}
                                 type="primary" htmlType="submit">
-                                Đăng ký
+                                {isLoading ? <LoadingOutlined spin /> : ' Đăng nhập'}
                             </Button>
                         </Col>
 
